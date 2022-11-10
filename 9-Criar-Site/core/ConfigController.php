5b9fe577d48b35c6a91fe5a17750f6560186334c
Sergio Pelegrini <sergio.pelegrini@scorpios.com.br>
2022-11-10 16:59:17 -0300

    atualizacao

--- 9-Criar-Site/core/ConfigController.php
+++ 9-Criar-Site/core/ConfigController.php
@@ -5,6 +5,12 @@ namespace Core;
 class ConfigController
 {
     private string $url;
+
+    private array $urlArray;
+    private string $urlController;
+    private string $urlParameter;
+    private string $urlSlugController;
+    private array $format;
     
     public function __construct()
     {
@@ -15,14 +21,36 @@ class ConfigController
             // http://localhost/php-developer-celke/9-Criar-Site/artigos/index?situacao=1&origem=email
             //var_dump($this->url); //aqui pega "artigos/index"
             //http://localhost/php-developer-celke/9-Criar-Site/contato
+            echo '<pre>';
             var_dump($this->url); //pega a página contato
+            echo '</pre>';
             //se quiser pegar o que vem depois:
             //$situacao = filter_input(INPUT_GET, 'situacao', FILTER_DEFAULT);
             //var_dump($situacao);
             //$origem = filter_input(INPUT_GET, 'origem', FILTER_DEFAULT);
             //var_dump($origem);
+
+            // transforma string em array com 2 posições separados pelo '/'
+            $this->urlArray = explode("/", $this->url);
+            echo '<pre>';
+            var_dump($this->urlArray);
+            echo '</pre>';
+
+            if (isset($this->urlArray[0])){
+                echo '<pre>';
+                var_dump($this->urlArray[0]);
+                echo '</pre>';
+                $this->urlController = $this->urlArray[0];
+            } else {
+                $this->urlController = "Home";
+            }
+
         } else {
             echo "Acessa a página inicial<br>";
+            $this->urlController = "Home";
         }
+
+        echo "Controller: {$this->urlController}<br>";
+
     }
 }
